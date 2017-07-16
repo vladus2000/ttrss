4767d4511ad2b8a574b33f9a1dc0047c63d7c23c
vladus2000 <docker@matt.land>
2017-07-16 13:53:04 -0500

    adding user manipulation scripts

--- shiz/startup.sh
+++ shiz/startup.sh
@@ -1,3 +1,6 @@
+/fixuser.sh mysql
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 	su mysql -c /usr/sbin/mysqld &
@@ -5,11 +8,11 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql -u root < /create
 	mysql --user ttrss --password=ttrss ttrss < /usr/share/webapps/tt-rss/schema/ttrss_schema_mysql.sql
 else
-	chown -R mysql /var/lib/mysql
 	su mysql -c /usr/sbin/mysqld &
 fi
 php-fpm
 nginx
+sleep 3
 while true
 do
 	su -s /bin/bash http -c '/usr/bin/php /usr/share/webapps/tt-rss/update.php --daemon'
